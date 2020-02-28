e211550860afe0b7b8adb35b98d5e1fbf71ebc1b
cruzortiz99 <cruzortiz099@gmail.com>
2020-02-28 17:21:40 -0400

    * adding loader in register and login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,19 +1,26 @@
 import { analytics as banner } from 'ionicons/icons'
-import React from 'react'
+import React, { useState } from 'react'
 import { loginUser } from '../../api/api'
 import LoginForm from '../../components/basics/LoginForm/LoginForm'
 import LoginContainer from '../../components/containers/LoginContainer/LoginContainer'
 import './Login.css'
+import { IonLoading } from '@ionic/react'
 
-const login = (email: string, password: string) => {
-  loginUser(email, password)
+const login = async (email: string, password: string) => {
+  return await loginUser(email, password)
 }
 
 const AppLogin: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false)
   return (
     <LoginContainer banner={banner}>
+      <IonLoading isOpen={isLoading} message='Please wait' />
       <LoginForm
-        onSave={(email: string, password: string) => login(email, password)}
+        onSave={async (email: string, password: string) => {
+          setIsLoading(true)
+          await login(email, password)
+          setIsLoading(false)
+        }}
       ></LoginForm>
     </LoginContainer>
   )
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,11 +1,12 @@
+import { IonLoading } from '@ionic/react'
 import { person as banner } from 'ionicons/icons'
-import React from 'react'
-import RegisterForm from '../../components/basics/RegisterForm/RegisterForm'
-import LoginContainer from '../../components/containers/LoginContainer/LoginContainer'
-import { toast } from '../../components/basics/utils/AppToast'
+import React, { useState } from 'react'
 import { registerUser } from '../../api/api'
+import RegisterForm from '../../components/basics/RegisterForm/RegisterForm'
+import { toast } from '../../components/basics/utils/AppToast'
+import LoginContainer from '../../components/containers/LoginContainer/LoginContainer'
 
-const onSaveHandler = (
+const onSaveHandler = async (
   error: string,
   data: {
     userName: string | undefined
@@ -16,15 +17,21 @@ const onSaveHandler = (
     return toast(error, undefined, 'danger', undefined)
   }
   return data.userName && data.userPassword
-    ? registerUser(data.userName, data.userPassword)
+    ? await registerUser(data.userName, data.userPassword)
     : null
 }
 
 const AppRegisterPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
   return (
     <LoginContainer banner={banner}>
+      <IonLoading isOpen={isLoading} message='Please wait'></IonLoading>
       <RegisterForm
-        onSave={(error, data) => onSaveHandler(error, data)}
+        onSave={async (error, data) => {
+          setIsLoading(true)
+          await onSaveHandler(error, data)
+          setIsLoading(false)
+        }}
       ></RegisterForm>
     </LoginContainer>
   )
