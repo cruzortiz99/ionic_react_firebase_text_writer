ab6dbe1496746b860c98bea4d027908d9aec6feb
cruzortiz99 <cruzortiz099@gmail.com>
2020-03-04 11:33:18 -0400

    text writer ionic

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -16,11 +16,24 @@ import AppLoader from '../../components/basics/AppLoader'
 import words from '../../data/words'
 import './Dashboard.css'
 
+type WordType = {
+  word: string
+  done: boolean
+  correct: boolean
+}
+
 const AppDashboard: React.FC = () => {
   const user = useSelector((state: any) => state.user)
   const history = useHistory()
   const [busy, setBusy] = useState(false)
   const [textWrote, setTextWrote] = useState('')
+  const [activeWordList, setActiveWordList] = useState<WordType[]>(
+    words
+      .slice(0, 10)
+      .map(word => ({ word: word, done: false, correct: false }))
+  )
+  const [activeWordIndex, setActiveWordIndex] = useState(0)
+  const [removeIndex, setRemoveIndex] = useState(0)
 
   const userLogout = async () => {
     setBusy(true)
@@ -32,8 +45,29 @@ const AppDashboard: React.FC = () => {
     const eventValue: string = event.target.value
     if (!eventValue.trim()) {
       setTextWrote('')
-    } else if (eventValue.slice(-1) === ' ') {
+    } else if (eventValue.slice(-1).match(/\s/)) {
       // do some processing
+      setActiveWordList(list => {
+        let wordsBlock = [...list]
+        wordsBlock[activeWordIndex] = {
+          word: '',
+          ...wordsBlock[activeWordIndex],
+          done: true,
+          correct: wordsBlock[activeWordIndex].word === eventValue.trim()
+        }
+        debugger
+        if (wordsBlock.length > 11) {
+          wordsBlock[removeIndex] = { word: '', correct: false, done: false }
+          setRemoveIndex(current => ++current)
+        }
+        setActiveWordIndex(activeIndex => ++activeIndex)
+        wordsBlock.push({
+          word: words[wordsBlock.length] || eventValue.trim(),
+          correct: false,
+          done: false
+        })
+        return wordsBlock
+      })
       setTextWrote('')
     } else {
       setTextWrote(eventValue)
@@ -49,11 +83,21 @@ const AppDashboard: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-        {words.map((word: string, key: number) => (
-          <span className='dashboard-words' key={key}>
-            {word}
-          </span>
-        ))}
+        {activeWordList
+          .filter(word => !!word.word)
+          .map((wordType: any, key: number) => {
+            const activeClass =
+              wordType.done && wordType.correct
+                ? 'dashboard-word-done dashboard-word-correct'
+                : wordType.done && !wordType.correct
+                ? 'dashboard-word-done dashboard-word-incorrect'
+                : ''
+            return (
+              <span className={`dashboard-words ${activeClass}`} key={key}>
+                {wordType.word}
+              </span>
+            )
+          })}
         <IonInput
           placeholder='Write the word'
           value={textWrote}
