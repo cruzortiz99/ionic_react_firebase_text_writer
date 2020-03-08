ac2e0ad79b5db000cc57c1f645b53e0b91aa145f
cruzortiz99 <cruzortiz099@gmail.com>
2020-03-08 08:07:20 -0400

    removing security from the repository

--- src/api/firebase.config.ts
+++ src/api/firebase.config.ts
@@ -1,13 +1,4 @@
 import * as firebase from 'firebase'
+import firebaseConfig from './firebase.config.json'
 
-const config = {
-  apiKey: 'AIzaSyDFH1jdTpoulHpCt3ENt1B9GCW8f_JGHQ0',
-  authDomain: 'ionic-react-app-f0b88.firebaseapp.com',
-  databaseURL: 'https://ionic-react-app-f0b88.firebaseio.com',
-  projectId: 'ionic-react-app-f0b88',
-  storageBucket: 'ionic-react-app-f0b88.appspot.com',
-  messagingSenderId: '475523760048',
-  appId: '1:475523760048:web:7af86d5f20b2d18e676075'
-}
-
-export default firebase.initializeApp(config)
+export default firebase.initializeApp(firebaseConfig)
